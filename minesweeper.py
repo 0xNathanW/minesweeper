1d7766e43e894e530c1dd7f15e67fc86adb39274
BigBadWolf <86011312+0x-Nathan@users.noreply.github.com>
2021-08-18 03:42:03 +0100

    Fully fleshed out, almost at finished version.
    //NathanW

--- minesweeper.py
+++ minesweeper.py
@@ -1,30 +1,34 @@
 import numpy as np
 import random
+import os
 
 
 class MineSweeper:
 
-    #   Probability of a square being a bomb.
-    p = 0.15
+    #   Probability of a cell being a mine.
+    p = 0.2
     #   Game state.
     finished = False
+    #   Number of mines on board.
+    mine_count = 0
 
     #   Construct an array from given grid parameters.
-    #   Construct grid that will be shown to player.
+    #   Construct the user_grid that will be shown to player.
     def __init__(self, grid_size):
         self.grid = np.array([[0 for i in range(grid_size[0])] for j in range(grid_size[1])])
-        self.user_grid = np.array([["#" for i in range(grid_size[0])] for j in range(grid_size[1])])
+        self.user_grid = np.array([[" " for i in range(grid_size[0])] for j in range(grid_size[1])])
         self.rows = grid_size[1]
         self.cols = grid_size[0]
 
-    #   Initialise the grid with bombs denoted by -99.
-    #   Calculate number of bombs in non-bomb cells' perimeters.
-    #   Reformat array to strings.
+    #   Load grids with mines, temporarily are -99.
+    #   Cells with mines add 1 to adjacent cells.
+    #   Reformat numerical array to strings.
     def init_game(self):
         for y in range(self.rows):
             for x in range(self.cols):
                 if random.random() < self.p:
                     self.grid[y][x] = -99
+                    self.mine_count += 1
                     for i in range(-1, 2):
                         for j in range(-1, 2):
                             try:
@@ -37,48 +41,94 @@ class MineSweeper:
             for x in range(self.cols):
                 display[y][x] = "X" if self.grid[y][x] < -50 else str(self.grid[y][x])
         self.grid = display
-        #print(self.grid)
 
+    #   Print user_array in current state to terminal.
     def display_user(self):
-        print("\t\t" + "\t\t".join([str(i) for i in range(1, self.cols + 1)]))
-        print("-"*((8*self.cols)+3))
+        print("%"*((8*self.cols)+3))
+        print("\t|\t" + "\t|\t".join([str(i) for i in range(1, self.cols + 1)]))
+        print("_"*((8*self.cols)+3))
         for row_num, row in enumerate(self.user_grid):
             if row_num != 0:
-                print(" \t|")
-            print(str(row_num+1) + "\t|\t" + "\t\t".join([cell for cell in row]))
+                print("-"*((8*self.cols)+3))
+            print(str(row_num+1) + "\t|\t" + "\t|\t".join([cell for cell in row]))
+        print("%" * ((8 * self.cols) + 3))
+        print("\n")
 
+    #   Flag action, replaces cell in user_grid with "F"
     def flag(self, x, y):
-        self.user_grid[y][x] = "F"
-    #TODO Handle for flagging cells already with flags
+        if self.user_grid[y][x] == "F":
+            print("This cell has already been flagged.\n")
+        else:
+            self.user_grid[y][x] = "F"
 
+    #   Reveal action, set cell in user_grid to equivalent cell in grid.
     def reveal(self, x, y):
         self.user_grid[y][x] = self.grid[y][x]
+        #   If cell is mine, lose game.
         if self.user_grid[y][x] == "X":
-            pass
+            self.game_lost()
+        #   If cell has 0 adjacent mines, reveal all surrounding cells.
+        #   Furthermore if adjacent cells are also 0, recursively reveal them until
+        #   no more 0 cells are adjacent.
         elif self.user_grid[y][x] == "0":
-            print(f"{x}, {y} is 0")
+            adj_not_revealed = 0
             for i in range(-1, 2):
                 for j in range(-1, 2):
                     try:
                         if (y + i) >= 0 and (x + j) >= 0:
-                            self.user_grid[y+i][x+j] = self.grid[y+i][x+j]
                             if i != 0 or j != 0:
-                                print(self.grid[y+i][x+j])
-                                if self.grid[y+i][x+j] == "0" and self.user_grid[y+i][x+j] == "#":
-                                    print(f"({y+i}, {x+j}) also 0")
-                                    print(self.user_grid)
-                                    self.reveal((y+i),(x+j))
-                        else:
-                            print("Conditions not met")
+                                if self.user_grid[y+i][x+j] == " ":
+                                    adj_not_revealed += 1
+                                    self.user_grid[y+i][x+j] = self.grid[y+i][x+j]
                     except IndexError:
                         pass
+            if adj_not_revealed > 0:
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        try:
+                            if (y + i) >= 0 and (x + j) >= 0:
+                                if i != 0 or j != 0:
+                                    if self.grid[y+i][x+j] == "0":
+                                        self.reveal((x+j), (y+i))
+                                    else:
+                                        self.user_grid[y+i][x+j] = self.grid[y+i][x+j]
+                        except IndexError:
+                            pass
         else:
             pass
 
+    def play_again(self):
+        again = input("Play again? (Y/N)").upper()
+        if again == "Y":
+            main()
+        if again == "N":
+            exit()
+        else:
+            print("Invalid answer.")
+            self.play_again()
 
+    #   Show all mines if game lost.
+    def game_lost(self):
+        self.finished = True
+        for y in range(self.rows):
+            for x in range(self.cols):
+                if self.grid[y][x] == "X":
+                    self.user_grid[y][x] = "X"
+        self.display_user()
+        self.play_again()
+
+    def check_win(self):
+        successful_flags = 0
+        for y in range(self.rows):
+            for x in range(self.cols):
+                if self.user_grid[y][x] == "F" and self.grid[y][x] == "X":
+                    successful_flags += 1
+        if successful_flags == self.mine_count:
+            self.finished = True
+            print("Congrats, you won!!! You're prize is ... absolutely nothing.")
+            self.play_again()
 
     def user_move(self):
-        #   F for flag, R for reveal.
         valid_actions = ["F", "R"]
         valid = True
         user_input = input("Next move: ")
@@ -87,44 +137,72 @@ class MineSweeper:
             valid = False
         try:
             x, y, action = int(arr[0]) - 1, int(arr[1]) - 1, arr[2].upper()
-            if x > self.cols or y > self.rows:
+            if x + 1 > self.cols or y + 1 > self.rows:
                 valid = False
-        except ValueError:
+        except (ValueError, IndexError):
             valid = False
         if valid:
-            print("valid move")
-            print(f"At ({x}, {y}): {self.grid[y][x]}")
-            self.user_move()
+            if action == "F":
+                self.flag(x, y)
+            if action == "R":
+                self.reveal(x, y)
 
+            self.check_win()
         else:
             print("Not a valid move. Try again.")
             self.user_move()
 
-
+#   Retrieve grid size.
 def query_difficulty():
     difficulty_to_grid_size = {"easy": (5, 5), "medium": (10, 8), "hard": (18, 14)}
     user_choice = input("Choose a difficulty: easy, medium, or hard...").lower()
     if user_choice in difficulty_to_grid_size.keys():
+        print(difficulty_to_grid_size[user_choice])
         return difficulty_to_grid_size[user_choice]
     else:
         print("Invalid choice, please type easy, medium or hard.")
-        query_difficulty()
+        return query_difficulty()
 
 
 def main():
-    #grid_size = query_difficulty()
-    game = MineSweeper((5, 5))
+    grid_size = query_difficulty()
+    game = MineSweeper(grid_size)
     game.init_game()
-    #game.display_user()
-    # while game.finished is False:
-    #     game.user_move()
-    print(game.grid)
-    game.reveal(1, 1)
     game.display_user()
+    while game.finished is False:
+        game.user_move()
+        game.display_user()
 
 
 if __name__ == "__main__":
 
+    print("""
+            *******************************
+                Welcome to Minesweeper
+            *******************************
+
+How to play:
+    - Select a level of difficulty.
+    - The board starts completely concealed.
+    - The board is randomly scattered with mines, denoted by "X".
+    - Cells without mines contain a number which shows the number of mines in adjacent cells.
+    
+    - You have the option to either reveal cells, or flag them if you think they are a mine.
+    - Flagged cells are denoted by "F".
+    
+IMPORTANT:
+    - To make your move you type 3 charactors:
+        1. the x coordinate (shown at top of grid)
+        2. the y coordinate (shown to left of grid)
+        3. Either "R" to reveal, or "F" to flag.
+        
+    - You lose the game if you reveal a mine.
+    - You win the game by correctly flagging all mines.
+    
+""")
+
     main()
 
-#TODO Show all mines when game lost
+# TODO: Adjust probability of mines
+# TODO: Add option to reset when asked for move
+# TODO: Allow to select already revealed and reveal all if correct numb of mines
